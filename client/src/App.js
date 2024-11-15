8ac4d08882d217b5c647352be02a797e0daaf21c
CHEN-Ziyang-Rocky <chenziyangrocky@gmail.com>
2024-11-15 19:41:02 +0800

    czy

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+
+import React from 'react';
+import RegistrationForm from './components/RegistrationForm';
+
+function App() {
+  return (
+    <div>
+      <RegistrationForm />
+    </div>
+  );
+}
+
+export default App;
